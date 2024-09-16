cf978f8ad114880d24adafbb7c9e2319d7990ff3
Elvis Ezimako <ezimakoelvis@gmail.com>
2024-09-16 16:10:18 +0100

    commented out lines and unused variables

--- sandbox.js
+++ sandbox.js
@@ -1,9 +1,23 @@
-let firstName = 'Elvis';
-let lastName = 'Ezimako';
+// let firstName = 'Elvis';
+// let lastName = 'Ezimako';
 
-let fullName = firstName + " " + lastName;
+// let fullName = firstName + " " + lastName;
 
-console.log(fullName.toUpperCase());
+// console.log(fullName.toUpperCase());
 
-answer = fullName.toLowerCase();
-console.log(answer);
+// answer = fullName.toLowerCase();
+// console.log(answer);
+
+// let likes = 10;
+
+// likes += 20;
+// likes++;
+// likes -= 5;
+
+// likes *= 2;
+
+// likes /= 2;
+
+// console.log(likes);
+// let result = ("The post  has " + likes + " likes");
+// console.log(result);
