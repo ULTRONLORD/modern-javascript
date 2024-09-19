d26b9f0db68c599121cfdfa55c50b6383ef5510d
Elvis Ezimako <ezimakoelvis@gmail.com>
2024-09-19 22:21:17 +0100

    conversion

--- sandbox.js
+++ sandbox.js
@@ -1,19 +1,5 @@
-//booleans and comparisons
-console.log(true, false, "true", "false");
+// type conversion
+let score = "100";
+score = Number(score);
 
-//methods that can return booleans
-let email = "ezimakoelvis@gmail.com";
-let names = ["Elvis", "Mitchelle", "Gabriella", "Michael"];
-
-// let result = email.includes("e");
-//let result = names.includes("Gabriella");
-result = 50;
-age = 50;
-
-mern = (result!=age);
-
-console.log(mern);
-
-// comparison operators
-
-
+console.log(typeof score, score);
