e70dbf046429a945519dd55739ca891d3831359a
Elvis Ezimako <ezimakoelvis@gmail.com>
2024-09-20 02:25:55 +0100

    modified:   sandbox.js

--- sandbox.js
+++ sandbox.js
@@ -5,8 +5,17 @@
 
 // console.log('loop agula');
 
-const names = ['Nellie', 'Bob', 'Patrick'];
+// const names = ["Nellie", "Bob", "Patrick"];
 
-for (let i = 0; i < names.length; i++) {
-  console.log(names[i]);
+// for (let i = 0; i < names.length; i++) {
+//   // console.log(names[i]);
+//   let html = `<div>${names[i]}</div>`;
+//   console.log(html);
+// }
+
+//while loop
+let i = 0;
+while (i < 5) {
+  console.log("infinity: ", i);
+  i++;
 }
