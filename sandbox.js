d45c07f8e5fdf0649e8b64ebb0e149d7c3e3f7f7
Elvis Ezimako <ezimakoelvis@gmail.com>
2024-09-20 02:47:48 +0100

    Do while loop

--- sandbox.js
+++ sandbox.js
@@ -1,21 +1,6 @@
-// //control flow (Loops)
-// for (let i = 0; i <= 5; i++) {
-//   console.log('in loop: ', i);
-// }
+let i = 2;
 
-// console.log('loop agula');
-
-// const names = ["Nellie", "Bob", "Patrick"];
-
-// for (let i = 0; i < names.length; i++) {
-//   // console.log(names[i]);
-//   let html = `<div>${names[i]}</div>`;
-//   console.log(html);
-// }
-
-//while loop
-let i = 0;
-while (i < 5) {
-  console.log("infinity: ", i);
+do {
+  console.log("i is equal to:", i);
   i++;
-}
+} while (i < 5);
