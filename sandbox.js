508970f9fe169d5358c8bf6a40719e21e1db7c16
Elvis Ezimako <ezimakoelvis@gmail.com>
2024-09-20 02:13:07 +0100

    featched data from an array with a for loope

--- sandbox.js
+++ sandbox.js
@@ -1,4 +1,12 @@
-//control flow (Loops)
-for (let i = 0; i <= 5; i++) {
-  console.log("in loop: ", i);
+// //control flow (Loops)
+// for (let i = 0; i <= 5; i++) {
+//   console.log('in loop: ', i);
+// }
+
+// console.log('loop agula');
+
+const names = ['Nellie', 'Bob', 'Patrick'];
+
+for (let i = 0; i < names.length; i++) {
+  console.log(names[i]);
 }
