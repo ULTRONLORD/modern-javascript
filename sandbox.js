48a993f8500b48fd30981bb6acee3a926a3cbd6c
Elvis Ezimako <ezimakoelvis@gmail.com>
2024-09-23 00:08:52 +0100

    if else statements

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,8 @@
 //if statements
 const age = 72;
 
-if(age > 60){
+if (age >= 60) {
   console.log("You are over the allowed age which is 60");
+} else {
+  console.log("You below the maximum age so you can keep working");
 }
