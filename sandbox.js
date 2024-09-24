cca74669895588c18a0006cff1f3d43643ea22e8
Elvis Ezimako <ezimakoelvis@gmail.com>
2024-09-24 23:42:18 +0100

    rush

--- sandbox.js
+++ sandbox.js
@@ -1,1 +1,13 @@
-//
+//Logical Operators - OR || and AND &&
+
+const password = "p@ssword12345";
+
+if (password.length >= 12 && password.includes("@")) {
+  console.log("That is a very strong password well done");
+} else if (password.length >= 8) {
+  console.log("the password is long enough but not strong enough");
+} else {
+  console.log("password is not long enough");
+}
+
+//29:06 time for tut  video
