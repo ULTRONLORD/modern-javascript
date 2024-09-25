03af1d75063fe34a5575b4439c0e33f52e95d2bd
Elvis Ezimako <ezimakoelvis@gmail.com>
2024-09-25 23:20:54 +0100

    Inmplemented double operators on a singlel ine

--- sandbox.js
+++ sandbox.js
@@ -1,11 +1,14 @@
 //Logical Operators - OR || and AND &&
 
-const password = "p@ssword12345";
+const password = "p@ss";
 
 if (password.length >= 12 && password.includes("@")) {
   console.log("That is a very strong password well done");
-} else if (password.length >= 8) {
-  console.log("the password is long enough but not strong enough");
+} else if (
+  password.length >= 8 ||
+  (password.includes("@") && password.length <= 5)
+) {
+  console.log("the password is not long enough but strong");
 } else {
   console.log("password is not long enough");
 }
