da3b398a6ae1d8a50e89c4b4e52f668876137d06
Elvis Ezimako <ezimakoelvis@gmail.com>
2024-09-26 23:51:53 +0100

    NOT logical operator

--- sandbox.js
+++ sandbox.js
@@ -1,16 +1,2 @@
-//Logical Operators - OR || and AND &&
+//Logicsal NOT(!)
 
-const password = "p@ss";
-
-if (password.length >= 12 && password.includes("@")) {
-  console.log("That is a very strong password well done");
-} else if (
-  password.length >= 8 ||
-  (password.includes("@") && password.length <= 5)
-) {
-  console.log("the password is not long enough but strong");
-} else {
-  console.log("password is not long enough");
-}
-
-//29:06 time for tut  video
