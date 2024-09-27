2c95277a47415011b0aa6aab991e41e1a1291970
Elvis Ezimako <ezimakoelvis@gmail.com>
2024-09-27 18:22:29 +0100

    Implemented BREAK and CONTINUE in a loop

--- sandbox.js
+++ sandbox.js
@@ -1,2 +1,12 @@
-//Logicsal NOT(!)
+//break and continue
 
+const scores = [50, 30, 45, 100, 20, 10, 200];
+
+for (let i = 0; i < scores.length; i++) {
+  console.log("your score: ", scores[i]);
+
+  if (scores[i] === 100) {
+    console.log("congrats this is the highest number");
+    break;
+  }
+}
