6ee20d9fe7c5e775fe3e2120e6bb3b698fa10d05
Elvis Ezimako <ezimakoelvis@gmail.com>
2024-10-03 13:45:28 +0100

    reusing functions

--- sandbox.js
+++ sandbox.js
@@ -1,13 +1,21 @@
 //Functions and Methods
 
 //function declaratiion
-function greet() {
-  console.log("Hey there I'm quite hungry");
-}
+// function greet() {
+//   console.log("Hey there I'm quite hungry");
+// }
 
 //Function Expression
-const speak = function (name = "Elvis", time = "Afternoon") {
-  console.log(`Good ${time} ${name}`);
+// const speak = function (name = "Elvis", time = "Afternoon") {
+//   console.log(`Good ${time} ${name}`);
+// };
+
+// speak("Prof Elvis", "Evening");
+
+const calcArea = function (radius) {
+  let area = 3.14 * radius ** 2;
+  return area;
 };
 
-speak("Prof Elvis", "Evening");
+const area = calcArea(5);
+console.log(area);
