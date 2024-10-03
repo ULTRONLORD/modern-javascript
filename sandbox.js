7991f9e67eb68555602bb34d338c4fda3f861234
Elvis Ezimako <ezimakoelvis@gmail.com>
2024-10-03 12:30:54 +0100

    using functions and methods

--- sandbox.js
+++ sandbox.js
@@ -1,26 +1,13 @@
-//switch statements
+//Functions and Methods
 
-const grade = "A";
-
-switch (grade) {
-  case "A":
-    console.log("You got an A!");
-    break;
-  case "B":
-    console.log("You got a B!");
-    break;
-  case "C":
-    console.log("You got a C!");
-    break;
-  case "D":
-    console.log("You got a D!");
-    break;
-  case "E":
-    console.log("You got an E!");
-    break;
-  case "F":
-    console.log("You got an F!");
-    break;
-  default:
-    console.log("not a valid grade");
+//function declaratiion
+function greet() {
+  console.log("Hey there I'm quite hungry");
 }
+
+//Function Expression
+const speak = function () {
+  console.log("I'll be going to cafe soon");
+};
+
+speak();
