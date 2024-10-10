a105fbb124a2cecadbd946ba23d26bedcf15fcde
Elvis Ezimako <ezimakoelvis@gmail.com>
2024-10-10 16:52:53 +0100

    exaples

--- sandbox.js
+++ sandbox.js
@@ -1,10 +1,9 @@
-// practising arrow functions
+const bill = (products, tax) => {
+  let total = 0;
+  for(let i = 0; i < products.lenght; i++){
+    total += products[i] + products[i] * tax;
+}
+return total;
+};
 
-// const greet = function () {
-//   return "Hello World!";
-// };
-
-const greet = () => "Hello, world!";
-const result = greet();
-
-console.log(result);
+console.log(bill([10,15,30], 0.2));
